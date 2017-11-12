2a667fbf6a87231d6bae9cf1d60d5cb520631e34
marcelofaria <marceloasf952@gmail.com>
2017-11-12 00:08:43 -0200

    Adicionando funcao add em TestingSDAO + algumas correcoes

--- GPROTool/src/model/TestingSDAO.java
+++ GPROTool/src/model/TestingSDAO.java
@@ -5,7 +5,13 @@
  */
 package model;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Map;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,7 +22,8 @@ public class TestingSDAO extends SDAO<Testing>{
     public static TestingSDAO instance;
     private final String fileName = "testing.ser";
     
-    private TreeMap<String, Testing> testing;
+    
+    private TreeMap<TestingStint[], Testing> testing;
     
     public TestingSDAO(){
         this.testing = new TreeMap<>();
@@ -33,12 +40,22 @@ public class TestingSDAO extends SDAO<Testing>{
             
     @Override
     public Testing get(Object o) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(o instanceof Testing){
+            Testing t = (Testing) o;
+            for(Map.Entry<TestingStint[], Testing> entry : this.testing.entrySet()){
+                if(entry.getValue().getTestingWeather()== t.getTestingWeather()){
+                    return entry.getValue();
+                    
+                }
+            }
+            return null;
+        }
+        return null;
     }
 
     @Override
     public void delete(Testing b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
@@ -48,11 +65,37 @@ public class TestingSDAO extends SDAO<Testing>{
 
     @Override
     public void add(Testing b) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Testing t = (Testing) b;
+        if(!this.testing.isEmpty()){
+            for(Map.Entry<TestingStint[], Testing> entry : this.testing.entrySet()){
+                if(entry.getValue().getTestingWeather()== t.getTestingWeather()){
+                    return;
+                }
+            }
+            //stints eh do tipo TestingStints[]
+            this.testing.put(b.getStints(), t);
+            try{
+                FileOutputStream fileOut = new FileOutputStream(this.fileName);
+                ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                out.writeObject(this.testing);
+                fileOut.close();
+            }catch(IOException ex){
+                Logger.getLogger(TestingSDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }   
+        }
+        this.testing.put(b.getStints(), t);
+            try{
+                FileOutputStream fileOut = new FileOutputStream(this.fileName);
+                ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                out.writeObject(this.testing);
+                fileOut.close();
+            }catch(IOException ex){
+                Logger.getLogger(TestingSDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
 
     private void getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
     
