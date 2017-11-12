97c92f36539031c020284495d4cc28cafdeab08e
marcelofaria <marceloasf952@gmail.com>
2017-11-12 00:23:28 -0200

    Adicionando funcao getAll em TestingSDAO

--- GPROTool/src/model/TestingSDAO.java
+++ GPROTool/src/model/TestingSDAO.java
@@ -5,8 +5,13 @@
  */
 package model;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Map;
 import java.util.TreeMap;
@@ -95,6 +100,27 @@ public class TestingSDAO extends SDAO<Testing>{
     }
 
     private void getAll() {
+        try{
+            FileInputStream fileIn = new FileInputStream(this.fileName);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            TreeMap<TestingStint[], Testing> testingFile;
+            testingFile = (TreeMap<TestingStint[], Testing>) in.readObject();
+            fileIn.close();
+            in.close();
+            this.testing = testingFile;
+        } catch(EOFException | FileNotFoundException | ClassNotFoundException ex){
+            File f = new File(this.fileName);
+            try{
+                boolean createNewFile = f.createNewFile();
+                if(createNewFile){
+                    System.out.println("peguei aqui");
+                }
+            } catch(IOException exl) {
+                Logger.getLogger(TestingSDAO.class.getName()).log(Level.SEVERE, null, exl);
+            }
+        } catch(IOException ex){
+                Logger.getLogger(TestingSDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
